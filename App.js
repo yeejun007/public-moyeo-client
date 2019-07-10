2c4d9cf2768665d016cb458dd4aed8481bc12d75
yeejun007 <dlwns007@gmail.com>
2019-07-10 19:27:21 +0900

    refactor App.js on branch startup-page

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {};
