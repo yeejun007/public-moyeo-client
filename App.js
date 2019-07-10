0e52652270d038cc85bb0ae0e0b26511c856383a
yeejun007 <dlwns007@gmail.com>
2019-07-10 19:15:23 +0900

    add constructor to App.js

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <View style={styles.container}>
