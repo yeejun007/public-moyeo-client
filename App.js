bdd2e9018d7b12b77ae569e2235cd4a8fb72613f
yeejun007 <dlwns007@gmail.com>
2019-07-12 17:28:03 +0900

    delete comment for re pr

--- App.js
+++ App.js
@@ -28,13 +28,4 @@ class App extends Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
-
 export default App;
